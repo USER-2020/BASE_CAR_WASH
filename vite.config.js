fd088d4517b44e21f3176fb0f98edddd68109f4c
juanfernandozuluaga <hackingetico2021@gmail.com>
2024-08-29 05:53:19 -0500

    chore: Update proxy configuration to rewrite the request path for the backend API

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,9 @@ export default defineConfig({
     proxy: {
       '/api': {  // Ajusta esto a tu endpoint real
         target: 'https://apiun.controlsoftwarepro.com',  // Cambia esto a la URL de tu backend
-        changeOrigin: false,  // Cambia el origen de la solicitud para coincidir con el servidor de destino
+        changeOrigin: true,  // Cambia el origen de la solicitud para coincidir con el servidor de destino
         secure: true,  // Configura si el proxy debe aceptar certificados SSL auto-firmados
-        rewrite: (path) => path.replace(/^\/api/, ''),  // Reescribe la ruta de la solicitud si es necesario
+        rewrite: (path) => path.replace(/^\/api/, '/api'),  // Reescribe la ruta de la solicitud si es necesario
       },
     },
   },
