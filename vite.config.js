019f8ce890949169ac7f159542f43c8c5940351e
juanfernandozuluaga <hackingetico2021@gmail.com>
2024-08-29 07:22:41 -0500

    chore: Update server configuration, Dockerfile, and nginx.conf

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '185.139.1.59', // Tu dirección IP específica
-    port: 5374,           // El puerto que desees
-
-  },
 })
